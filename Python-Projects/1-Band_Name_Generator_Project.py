b8dc742b2a3ccc20bf3e7d51100e0828c1667eb3
Jlang27 <joseluisangulom27@gmail.com>
2025-03-25 20:17:00 -0500

    Creación y subida de los 3 primeros proyectos

---
+++ Python-Projects/1-Band_Name_Generator_Project.py
@@ -0,0 +1,4 @@
+print("Herramienta de ayuda para generar nombres para bandas.\n")
+Ciudad = input("Ingresar el nombre de su ciudad natal: ")
+mascota = input("Ingresar el nombre de una mascota que hayas tenido: ")
+print(f"Tu nombre para banda podría ser: {Ciudad} {mascota}")
